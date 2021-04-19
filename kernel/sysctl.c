539e4fb4a4075c39194e2a1b70dac9652735e289
root <root@pop-os.localdomain>
2021-04-19 08:16:37 +0800

    kernel: sysctl: block init tuning for these procnames

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -1605,7 +1605,7 @@ static struct ctl_table vm_table[] = {
 		.extra1		= &zero,
 	},
 	{
-		.procname	= "dirty_background_ratio",
+		.procname	= "dirty_background_ratio_nosys",
 		.data		= &dirty_background_ratio,
 		.maxlen		= sizeof(dirty_background_ratio),
 		.mode		= 0644,
@@ -1639,14 +1639,14 @@ static struct ctl_table vm_table[] = {
 		.extra1		= &dirty_bytes_min,
 	},
 	{
-		.procname	= "dirty_writeback_centisecs",
+		.procname	= "dirty_writeback_centisecs_nosys",
 		.data		= &dirty_writeback_interval,
 		.maxlen		= sizeof(dirty_writeback_interval),
 		.mode		= 0644,
 		.proc_handler	= dirty_writeback_centisecs_handler,
 	},
 	{
-		.procname	= "dirty_expire_centisecs",
+		.procname	= "dirty_expire_centisecs_nosys",
 		.data		= &dirty_expire_interval,
 		.maxlen		= sizeof(dirty_expire_interval),
 		.mode		= 0644,
@@ -1667,7 +1667,7 @@ static struct ctl_table vm_table[] = {
 		.proc_handler   = pdflush_proc_obsolete,
 	},
 	{
-		.procname	= "swappiness",
+		.procname	= "swappiness_nosys",
 		.data		= &vm_swappiness,
 		.maxlen		= sizeof(vm_swappiness),
 		.mode		= 0644,
