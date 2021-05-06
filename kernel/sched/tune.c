b9438f68aa29c96bab248dc00d830a610e845d59
Sergey Vasiliev <sdv22@live.ru>
2021-05-06 14:49:56 +0300

    Set default shedtune by cpuset

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -751,11 +751,11 @@ struct st_data {
 static void write_default_values(struct cgroup_subsys_state *css)
 {
 	static struct st_data st_targets[] = {
-		{ "audio-app",	0, 0 },
+		{ "audio-app",	1, 0 },
 		{ "background",	0, 0 },
-		{ "foreground",	0, 1 },
-		{ "rt",		0, 0 },
-		{ "top-app",	0, 1 },
+		{ "foreground",	0, 0 },
+		{ "rt",		1, 0 },
+		{ "top-app",	1, 0 },
 	};
 	int i;
 
