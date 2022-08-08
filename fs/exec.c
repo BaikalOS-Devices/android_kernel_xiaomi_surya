b7db02de557fe29aefa5779283ccc8ba00d5d7b9
Sergey Vasiliev <sdv22@live.ru>
2022-08-08 15:32:38 +0300

    fs: perfmask on some processes

--- fs/exec.c
+++ fs/exec.c
@@ -1834,29 +1834,27 @@ static int do_execveat_common(int fd, struct filename *filename,
 		goto out;
 
 	if (is_global_init(current->parent)) {
-		if (unlikely(!strncmp(filename->name,
+		/*if (unlikely(!strncmp(filename->name,
 					   HWDISPLAY_BIN_PREFIX,
 					   strlen(HWDISPLAY_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
 			//set_cpus_allowed_ptr(current, cpu_perf_mask);
             set_cpus_allowed_ptr(current, cpu_all_mask);
-		} else if (unlikely(!strncmp(filename->name,
+		} else */ if (unlikely(!strncmp(filename->name,
 			           HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
-			//set_cpus_allowed_ptr(current, cpu_perf_mask);
-            set_cpus_allowed_ptr(current, cpu_all_mask);
-		} else if (unlikely(!strncmp(filename->name,
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+            //set_cpus_allowed_ptr(current, cpu_all_mask);
+		} else  if (unlikely(!strncmp(filename->name,
 			           AUDIOSRVS_BIN_PREFIX,
 					   strlen(AUDIOSRVS_BIN_PREFIX)))) {
 			current->flags |= PF_PERF_CRITICAL;
-			//set_cpus_allowed_ptr(current, cpu_perf_mask);
-            set_cpus_allowed_ptr(current, cpu_all_mask);
-		}
-		else if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN))) {
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+            //set_cpus_allowed_ptr(current, cpu_all_mask);
+		} else if (unlikely(!strcmp(filename->name, ZYGOTE32_BIN))) {
 			zygote32_sig = current->signal;
-		}
-		else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN))) {
+		} else if (unlikely(!strcmp(filename->name, ZYGOTE64_BIN))) {
 			zygote64_sig = current->signal;
 		}
 	}
